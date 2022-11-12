94f17a85fc9083a8a0983dbd9492390194add27f
tipbco <114815069+tipbco@users.noreply.github.com>
2022-11-12 10:23:31 +0330

    ادمین پنل
    بروزرسانی ادمین پنل

---
+++ Metawision.Blog/Metawision.Blog/Models/mohsenAdminController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Metawision.Blog.Controllers
+{
+    public partial class AdminController : Controller
+    {
+        [HttpGet]
+        public ActionResult adminPanel()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public ActionResult _adminLayout( )
+        {
+            return View();
+        }
+    }
+}
